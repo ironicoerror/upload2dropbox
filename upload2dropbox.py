951a62837a39c55f11d91cbb11e83e32fd53648b
ironicoerror <rogalla.tobias@googlemail.com>
2020-01-31 09:55:52 +0100

    hotfix: added try statement to remove error occuring while no files are to be copied

--- upload2dropbox.py
+++ upload2dropbox.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import dropbox 
 import os
@@ -7,7 +7,7 @@ from shutil import move
 import datetime
 
 the_date = str(datetime.datetime.now())[0:10]
-log_path = r"/home/pi/Desktop/temperature_logs/waeschekeller_ug"
+log_path = r"/home/pi/Desktop/temperature_logs/waeschekeller_ug/"
 filelist = os.listdir(log_path)
 filelist.remove(the_date.replace("-", "") + ".log") #do not copy todays log it might be changed 
 
@@ -21,17 +21,19 @@ try:
     db_client = dropbox.Dropbox("zpAbOclWSRUAAAAAAAAEelZenv-Ae8W-zXeTeSUqdiVQNpb9bBbF3o8DHR3OVSGb")
     for files in filelist:
         if os.path.isdir(log_path + files) is False:
-            print("Uploading: " + files)
-            # open file
-            fileHandle = open(log_path + files, "rb")
-            fileBytes = fileHandle.read()
-            # upload file to dropbox
-            upload_response = db_client.files_upload(fileBytes, "/waeschekeller_ug/" + files, autorename=True)
-            move(log_path + files, arc_path)
-            print(upload_response)
+            try:
+                print("Uploading: " + files)
+                # open file
+                fileHandle = open(log_path + files, "rb")
+                fileBytes = fileHandle.read()
+                # upload file to dropbox
+                upload_response = db_client.files_upload(fileBytes, "/waeschekeller_ug/" + files, autorename=True)
+                move(log_path + files, arc_path)
+                print(upload_response)
+            finally:
+                fileHandle.close()
         else:
             print(files + " is a directory and not uploaded")
 except Exception as e:
     print(e)
-finally:
-    fileHandle.close()
+
